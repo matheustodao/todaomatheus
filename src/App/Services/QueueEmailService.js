f0c2a43a5fd50befb2a5715c86ed5ce8ddc58609
Matheus Todao <todaomatheus@hotmail.com>
2021-10-22 23:49:55 -0300

    "Removed" QueueEmailService

--- src/App/Services/QueueEmailService.js
+++ src/App/Services/QueueEmailService.js
@@ -1,12 +1,15 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable no-unused-vars */
 import HttpClient from './utils/HttpClient';
 
 class QueueEmailService {
   constructor() {
-    this.httpClient = new HttpClient('http://localhost:3001');
+    // this.httpClient = new HttpClient('http://localhost:3001');
   }
 
   async addNewQueueEmail(data) {
-    return this.httpClient.post('/queues/emails', data);
+    // return this.httpClient.post('/queues/emails', data);
+    return null;
   }
 }
 
