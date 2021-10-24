a6c3e8b83e225ba55f2e83e856319aed894397be
Matheus Todao <todaomatheus@hotmail.com>
2021-10-23 22:56:39 -0300

    Changed Head informations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,15 +9,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="light" />
+        <meta charset="UTF-8" />
+        <meta name="theme-color" media="light" content="#FFFFFF" />
+        <meta name="theme-color" media="dark" content="#FFFFFF" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta
           name="description"
-          content="Oi! Eu sou Matheus Todao, sou desenvolvedor ReactJS e NodeJS."
+          content="Oi! Eu sou <strong>Matheus Todao</strong>, sou desenvolvedor ReactJS, Javascript e NodeJS... Está precisando de uma Landing Page da para sua loja? Entra em contato!"
         />
         <meta name="msapplication-config" content="/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <title>Matheus Todao</title>
+        <meta name="google-site-verification" content="8UzdDI7o5Ekc81ZlXinATnvEJznzCDc7tR2HktWtdYE" />
+        <title>Matheus Todao - Desenvolvedor</title>
       </Head>
       <ModalProvider>
         <GlobalStyle />
