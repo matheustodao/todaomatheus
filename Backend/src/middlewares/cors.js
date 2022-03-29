e51c93ba39766466a6de1e01f8670c6a5da7e5b4
matheustodao <todaomatheus@hotmail.com>
2022-03-29 20:22:15 -0300

    removed backend

--- Backend/src/middlewares/cors.js
+++
@@ -1,6 +0,0 @@
-module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', 'POST');
-  response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-};
