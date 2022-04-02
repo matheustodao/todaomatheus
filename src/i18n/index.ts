73a398901293434307fa804ebdfd5a658740e45e
matheustodao <todaomatheus@hotmail.com>
2022-04-02 01:25:29 -0300

    detecting user language and load translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 import { resources } from './locales';
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  debug: true,
-});
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: true,
+  });
