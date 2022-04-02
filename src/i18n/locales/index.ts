bd3ec7a3b9a1b62e4439e6924a67f0068399c2ec
matheustodao <todaomatheus@hotmail.com>
2022-04-01 23:25:14 -0300

    set up and initialize i18n

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,5 +1,19 @@
+import { ITranslation } from './types/translation';
 import enLanguage from './en.json';
+import ptBRLanguage from './pt-br.json';
 
-export {
-  enLanguage,
+interface IResources {
+  [key: string]: {
+    translation: ITranslation
+  },
+}
+
+export const resources: IResources = {
+  en: {
+    translation: enLanguage,
+  },
+
+  ptBR: {
+    translation: ptBRLanguage,
+  },
 };
