cd697a5ef662e4bafbaad7eb4a572f731a1ad3b7
matheustodao <todaomatheus@hotmail.com>
2022-04-02 20:38:11 -0300

    change: name function ...from translationHTLMParse to translationHTMLMParse
    add type at translationHTMLMParse

--- src/hooks/useTranslate.ts
+++ src/hooks/useTranslate.ts
@@ -1,11 +1,14 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Normalize } from 'react-i18next';
 import ReactHTMLParse from 'react-html-parser';
+import { ITranslation } from '../i18n/locales/types/translation';
 
 export default function useTranslate() {
   const { t, i18n } = useTranslation();
 
   return {
-    translationHTLMParse: (getTranslation: string) => ReactHTMLParse(t(getTranslation)),
+    translationHTMLMParse: (getTranslation: Normalize<ITranslation>) => (
+      ReactHTMLParse(t(getTranslation))
+    ),
     translation: t,
     i18n,
   };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,14 @@ import { Main } from '../styles';
 import useTranslate from '../hooks/useTranslate';
 
 export default function Home() {
-  const { translationHTLMParse, i18n } = useTranslate();
+  const { translationHTMLMParse, i18n } = useTranslate();
   function handleChangeLanguage() {
     const currentLanguage = i18n.language;
     return currentLanguage === 'en' ? i18n.changeLanguage('pt') : i18n.changeLanguage('en');
   }
   return (
     <Main>
-      {translationHTLMParse('pages.aboutMe.description')}
+      {translationHTMLMParse('pages.aboutMe.description')}
       <button type="button" onClick={handleChangeLanguage}>
         Mudar para
         {' '}
