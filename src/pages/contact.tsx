40ec46de865313b8ccec6bd44a00b33219299ca4
matheustodao <todaomatheus@hotmail.com>
2022-05-29 19:43:52 -0300

    done/page: reach me out

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,27 @@
+import Image from 'next/image';
+
+import { Container, WayOfContact } from '@styles/pages/contacts';
+
+import { contacts } from '@mocks/contacts';
+
 export default function Contact() {
-  return '';
+  return (
+    <Container>
+      <h1>Reach Me</h1>
+
+      <div className="container__way__of__contact">
+        {contacts.map((contact) => (
+          <WayOfContact
+            key={contact.id}
+            href={contact.link}
+            bg={contact.bg}
+            target="_blank"
+          >
+            <Image width={32} height={32} src={contact.icon} />
+            {contact.name}
+          </WayOfContact>
+        ))}
+      </div>
+    </Container>
+  );
 }
