902870868dcbe074ce576db1caf287af8f015b8f
matheustodao <todaomatheus@hotmail.com>
2022-07-05 00:20:30 -0300

    style(github): variable enviroment style: change github client path

--- src/configs/apis/github.ts
+++ src/configs/apis/github.ts
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { GITHUB_TOKEN } from '@configs/environments';
 
 export const GithubClient = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+    Authorization: `bearer ${GITHUB_TOKEN}`,
   },
   cache: new InMemoryCache(),
 });
