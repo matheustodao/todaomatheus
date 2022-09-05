ecbf55b946ad56bb88be3058f5383e3700a70cf2
matheustodao <todaomatheus@hotmail.com>
2022-09-05 01:38:22 -0300

    refactor: add ssrMode
    refactor: delete no used variables environments

--- src/configs/apis/github.ts
+++ src/configs/apis/github.ts
@@ -1,10 +1,13 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { GITHUB_TOKEN } from '@configs/environments';
 
 export const GithubClient = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  headers: {
-    Authorization: `bearer ${GITHUB_TOKEN}`,
-  },
+  ssrMode: true,
+  link: createHttpLink({
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      Authorization: `bearer ${GITHUB_TOKEN}`,
+    },
+  }),
   cache: new InMemoryCache(),
 });
